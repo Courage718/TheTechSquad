abde392b77d3d0536cf3f0b25ea1cfb834efcfe6
sayman zaya <saymanzaya@oakland.edu>
2024-10-08 13:34:20 -0400

    quartz scheduler

--- src/main/java/the_tech_squads/ou/task_manager/MyTaskJob.java
+++ src/main/java/the_tech_squads/ou/task_manager/MyTaskJob.java
@@ -2,7 +2,6 @@ package the_tech_squads.ou.task_manager;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
 public class MyTaskJob implements Job {
 
