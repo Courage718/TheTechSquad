05834aa14a7b34ee5521629256b8d13104788d95
Irina_Vertiagina <ivertiagina@oakland.edu>
2024-10-13 13:55:56 -0400

    database controller added

--- src/main/java/the_tech_squads/ou/task_manager/WelcomeController.java
+++ src/main/java/the_tech_squads/ou/task_manager/WelcomeController.java
@@ -1,13 +1,18 @@
 package the_tech_squads.ou.task_manager;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class WelcomeController {
 
     @GetMapping("/hi")
     public String welcome(){
         return "sample_html_page";
     }
+
+
 }
