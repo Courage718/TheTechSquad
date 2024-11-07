2ac0b78bf3b005fad5af7e0a4955a8caaac9673c
Irina_Vertiagina <ivertiagina@oakland.edu>
2024-11-06 20:04:28 -0500

    css link fixed

--- src/main/java/the_tech_squads/ou/task_manager/nlp/nlpMain.java
+++ src/main/java/the_tech_squads/ou/task_manager/nlp/nlpMain.java
@@ -1,49 +1,49 @@
-package the_tech_squads.ou.task_manager.nlp;
-
-import opennlp.tools.doccat.DoccatModel;
-
-import java.io.IOException;
-
-public class nlpMain {
-    String userInput;// input string coming from web
-    String processedTask;//possible output
-
-    public void setUserInput(String userInput) {
-        this.userInput = userInput;
-    }
-    Pipeline pipeline = new Pipeline(userInput);
-    public String getProcessedTask(){
-        return processedTask;
-    }
-
-    public void runNLP() throws IOException{
-
-        TaskController controller = new TaskController();
-        String userInput = controller.addTask();
-        Pipeline pipeline = new Pipeline(userInput);
-
-        pipeline.trainDoccatModel();
-
-        String[] sentences = pipeline.breakSentences();
-
-        String outputPrefix = "";
-
-        for (String sentence : sentences) {
-
-            String[] tokens = pipeline.tokenize(sentence);
-
-            String[] POStags = pipeline.POSTag(tokens, pipeline.doccatModel);
-
-            String[] lemmatizedTokens = pipeline.lemmatizeTokens(tokens, POStags);
-
-            String category = pipeline.detectCategory(pipeline.doccatModel, lemmatizedTokens);
-
-            //get task prefix for specific task categories
-            outputPrefix = pipeline.prefix.get(category);
-
-        }
-
-    }
-
-}
-
+//package the_tech_squads.ou.task_manager.nlp;
+//
+//import opennlp.tools.doccat.DoccatModel;
+//
+//import java.io.IOException;
+//
+//public class nlpMain {
+//    String userInput;// input string coming from web
+//    String processedTask;//possible output
+//
+//    public void setUserInput(String userInput) {
+//        this.userInput = userInput;
+//    }
+//    Pipeline pipeline = new Pipeline(userInput);
+//    public String getProcessedTask(){
+//        return processedTask;
+//    }
+//
+//    public void runNLP() throws IOException{
+//
+//        TaskController controller = new TaskController();
+//        String userInput = controller.addTask();
+//        Pipeline pipeline = new Pipeline(userInput);
+//
+//        pipeline.trainDoccatModel();
+//
+//        String[] sentences = pipeline.breakSentences();
+//
+//        String outputPrefix = "";
+//
+//        for (String sentence : sentences) {
+//
+//            String[] tokens = pipeline.tokenize(sentence);
+//
+//            String[] POStags = pipeline.POSTag(tokens, pipeline.doccatModel);
+//
+//            String[] lemmatizedTokens = pipeline.lemmatizeTokens(tokens, POStags);
+//
+//            String category = pipeline.detectCategory(pipeline.doccatModel, lemmatizedTokens);
+//
+//            //get task prefix for specific task categories
+//            outputPrefix = pipeline.prefix.get(category);
+//
+//        }
+//
+//    }
+//
+//}
+//
