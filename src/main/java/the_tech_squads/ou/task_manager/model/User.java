037cd65e8cad7891f5ef8aa999319b7397983ca6
Irina_Vertiagina <ivertiagina@oakland.edu>
2024-11-08 20:54:27 -0500

    registration feature added(without password verification) save task to db added

--- src/main/java/the_tech_squads/ou/task_manager/model/User.java
+++ src/main/java/the_tech_squads/ou/task_manager/model/User.java
@@ -1,10 +1,5 @@
 package the_tech_squads.ou.task_manager.model;
-import jakarta.persistence.*;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
