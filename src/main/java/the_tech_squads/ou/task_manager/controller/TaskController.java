d01d7d5be4924254a177f40ff0ae06383e5b165c
Irina_Vertiagina <ivertiagina@oakland.edu>
2024-11-08 20:45:58 -0500

    registration feature added(without password verification) save task to db added

--- src/main/java/the_tech_squads/ou/task_manager/controller/TaskController.java
+++ src/main/java/the_tech_squads/ou/task_manager/controller/TaskController.java
@@ -1,33 +1,39 @@
 package the_tech_squads.ou.task_manager.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-//import the_tech_squads.ou.task_manager.nlp.nlpMain;
+import the_tech_squads.ou.task_manager.model.Task;
+import the_tech_squads.ou.task_manager.service.TaskService;
 
 @Controller
 public class TaskController {
 
-   //@Autowired
-//    private nlpMain nlpMain;
+   @Autowired
+   private TaskService taskService;
 
     @GetMapping("/task")
     public String inputTask(){
         return "userInputTask";
     }
+//    @PostMapping("/task")
+//    public String addTask(@RequestParam String userInput, Model model) {
+//        //nlpMain.setUserInput(userInput);
+//       // model.addAttribute("task", userInput);//test task in web
+//        System.out.println();
+//        return "bye";// This refers to html page
+//    }
     @PostMapping("/task")
-    public String addTask(@RequestParam String taskName,
-                          @RequestParam String taskDescription,
-                          @RequestParam String dueDate,
-                          @RequestParam String priority,
-                                      Model model) {
-        //nlpMain.setUserInput(userInput);
-        System.out.println(taskDescription);
-        System.out.println(dueDate);
-        System.out.println(priority);
-        model.addAttribute("task", taskName);//test task in web
-        return "bye";// This refers to html page
+    public Task saveTask(@RequestBody Task task) {
+        return taskService.save(task);
+
     }
+    @PostMapping(value = "/task", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String submitTask(@ModelAttribute Task task) {
+       taskService.save(task);
+        return "bye";
+    }
+
 }
