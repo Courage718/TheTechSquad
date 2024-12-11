4b3b089ac0e42bb15a8a457a0a9686e8d772c0c3
turtleman1278 <schen7812@gmail.com>
2024-12-11 13:22:35 -0500

    removed some lines

--- src/main/java/the_tech_squads/ou/task_manager/controller/nlpController.java
+++ src/main/java/the_tech_squads/ou/task_manager/controller/nlpController.java
@@ -13,21 +13,16 @@ public class nlpController {
     private final Pipeline pipeline;
 
     public nlpController() {
-        // Initialize your NLP pipeline here, if necessary
         this.pipeline = new Pipeline(null);
     }
 
     @PostMapping
     public NlpResponse processText(@RequestBody NlpRequest request) throws IOException {
-        // Use the NLP pipeline to process the input text
         String userInput = request.getText();
-
-        // Assuming your NLP pipeline processes the text and returns appropriate results
         String processedTask = (pipeline).processTask(userInput);
-        String date = pipeline.extractDate(userInput);  // This should be part of your NLP pipeline
-        String category = pipeline.detectCategory(userInput);  // Based on your NLP pipeline
+        String date = pipeline.extractDate(userInput);  
+        String category = pipeline.detectCategory(userInput);  
 
-        // Create and return the response
         return new NlpResponse(processedTask, date, category);
     }
 }
